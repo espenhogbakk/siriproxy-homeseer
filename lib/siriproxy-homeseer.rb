728aad3ed959f0abd19cd643fde614bcdd971c15
Espen Høgbakk <espen@hogbakk.no>
2012-11-11 15:52:15 +0100

    Add run event command

--- lib/siriproxy-homeseer.rb
+++ lib/siriproxy-homeseer.rb
@@ -1,17 +1,32 @@
 require 'cora'
 require 'siri_objects'
 require 'pp'
+require 'httparty'
 
 #######
 # Plugin to control Homeseer automation software.
 ######
 
 class SiriProxy::Plugin::Homeseer < SiriProxy::Plugin
-  def initialize(config)
-    #if you have custom configuration options, process them here!
+  attr_accessor :host
+
+  def initialize(config = {})
+    self.host = config["host"]
   end
 
   listen_for /run event (.*)/i do |event|
-    say "You want to run event: #{event}"
+    run_event(event)
   end
+
+  def run_event(event)
+    say "One moment while I run the event #{event}."
+
+    Thread.new {
+      page = HTTParty.get("http://#{self.host}/tenHsServer/tenHsServer.aspx?t=ab&f=RunEvent&d=#{event}").body rescue nil
+      say "There you go..."
+      request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+    }
+
+  end
+
 end

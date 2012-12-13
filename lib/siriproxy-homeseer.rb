a69bf179e72e5a3c44c71d6055971fa56e92c546
Espen Høgbakk <espen@hogbakk.no>
2012-12-13 22:56:02 +0100

    Enable turning off either a device or a room with the same command

--- lib/siriproxy-homeseer.rb
+++ lib/siriproxy-homeseer.rb
@@ -15,12 +15,14 @@ class SiriProxy::Plugin::Homeseer < SiriProxy::Plugin
     self.host = config["host"]
   end
 
+  # Running an event
   listen_for /run event (.*)/i do |event|
     say "Running event #{event}."
     event = TenHsServer::Event.run event
     request_completed
   end
 
+  # Turn on/off all lights
   listen_for /turn (on|off)(?: the)? lights/i do |action|
     if action == 'on'
       say "Lights on."
@@ -32,18 +34,36 @@ class SiriProxy::Plugin::Homeseer < SiriProxy::Plugin
     request_completed
   end
 
-  listen_for /turn (on|off) ([a-z]*)(?: in)?(?: the)? ([a-z]*)/i do |action, device_name, location|
-    room = TenHsServer::Room.new location
+  # Turn on/off a device in a specified room
+  listen_for /turn (on|off) ([a-z]*)(?: in)?(?: the)? ([a-z]*)/i do |action, device_name, room_name|
+    room = TenHsServer::Room.new room_name
 
     run_action_on_device action, room.devices, device_name
     request_completed
   end
 
-  listen_for /turn (on|off) ([a-z]*)(?: lights)?/i do |action, device_name|
+  # Turn on/off a device or a room
+  listen_for /turn (on|off) ([a-z]*)(?: lights)?/i do |action, name|
     devices = TenHsServer::Device.all
-    devices = devices.find_all {|device| device.name.downcase == device_name.downcase}
+    devices = devices.find_all {|device| device.name.downcase == name.downcase}
+    
+    # If we couldn't find any devices, try finding a room with that name
+    if devices.empty?
+      room = TenHsServer::Room.new name
+      if room
+        if action == 'on'
+          room.on
+        else
+          room.off
+        end
+        say "Turning #{action} #{name}."
+      else
+        say "Sorry, I couldn't find any devices, or rooms by that name."
+      end
+    else
+      run_action_on_device action, devices, name
+    end
 
-    run_action_on_device action, devices, device_name
     request_completed
   end
 
